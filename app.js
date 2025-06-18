be15fbba05965be5b4ca826c62360ff9999ee71c
Vraj Valand <123581157+Vraj-V@users.noreply.github.com>
2025-06-18 17:36:57 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,27 @@
+const secondHand = document.getElementById('second-hand');
+const minuteHand = document.getElementById('minute-hand');
+const hourHand = document.getElementById('hour-hand');
+
+function clockTick() {
+    const date = new Date();
+    const seconds = date.getSeconds() / 60;
+    const minutes = (seconds + date.getMinutes() ) / 60;
+    const hours = (minutes + date.getHours() ) / 12;
+
+    rotateClockHand(secondHand, seconds);
+    rotateClockHand(minuteHand, minutes);
+    rotateClockHand(hourHand, hours);
+}
+
+function rotateClockHand(element, rotation) {
+    element.style.setProperty('--rotate', rotation * 360);
+}
+
+setInterval(clockTick, 1000);
+let day = document.getElementById('day');
+day.innerHTML = "todays os new date", day.innerHTML = new Date().toLocaleDateString('en-US', {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric'
+});
